12393c063b52b16bdc12f7fbd6ba2b7f1a21c8d2
juan <juanlaranas@gmail.com>
2025-07-04 17:56:39 -0600

    Update models.py

--- models.py
+++ models.py
@@ -14,7 +14,7 @@ class AccountStatus(Base):
     __tablename__ = "account_status"
 
     account_number = Column(String, primary_key=True, index=True)
-    account_balance = Column(Float, default=0.0)
+    account_balance = Column(String, default="")
     last_trade = Column(String, default="")
     account_mode = Column(String, default="")
     broker_server = Column(String, default="")
